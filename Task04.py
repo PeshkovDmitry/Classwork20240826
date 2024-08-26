c29d8574e3d7e2691d4bf3438c0b50569da624b8
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-08-26 13:52:32 +0300

    Решена задача 4

--- Task04.py
+++ Task04.py
@@ -7,3 +7,37 @@
 ○ уровень доступа вычисляемый как остаток от деления суммы цифр id на семь
 """
 
+
+class Person:
+
+    def __init__(self, name: str, age: int, phone: str):
+        self.__name = name
+        self.__age = age
+        self.__phone = phone
+
+    def birthday(self):
+        self.__age += 1
+
+    def get_full_name(self):
+        return self.__name
+
+    def get_age(self):
+        return self.__age
+
+    def get_phone(self):
+        return self.__phone
+
+
+class Employee(Person):
+
+    def __init__(self, name: str, age: int, phone: str, id: int):
+        super().__init__(name, age, phone)
+        self.__id = id
+        self.__level = id % 7
+
+    def info(self):
+        return f"{self.__id} : ({self.__level}) {self.get_full_name()} / {self.get_age()} / {self.get_phone()}"
+
+
+e = Employee("Иван", 33, "1-234-567-8901", 123456)
+print(e.info())
