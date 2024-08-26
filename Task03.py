40906b5897f2b183a9b2ccd5dfc5a57eb8cf412f
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-08-26 13:44:59 +0300

    Решена задача 3

--- Task03.py
+++ Task03.py
@@ -7,3 +7,27 @@ full_name для вывода полного ФИО и т.п. на ваш выб
 но есть возможность получить текущий возраст.
 """
 
+class Person:
+
+    def __init__(self, name: str, age: int, phone: str):
+        self.__name = name
+        self.__age = age
+        self.__phone = phone
+
+    def birthday(self):
+        self.__age += 1
+
+    def get_full_name(self):
+        return self.__name
+
+    def get_age(self):
+        return self.__age
+
+    def get_phone(self):
+        return self.__phone
+
+
+p = Person("Иван", 33, "1-234-567-8901")
+print(f"{p.get_full_name()} / {p.get_age()} / {p.get_phone()}")
+p.birthday()
+print(f"{p.get_full_name()} / {p.get_age()} / {p.get_phone()}")
